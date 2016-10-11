22a49edf962efa4458a887c40ee45afe44110c3a
Asbjørn Nilsen Riseth <anriseth@gmail.com>
2016-10-11 09:25:53 +0100

    Separate out test file

---
+++ test/alphacalc.jl
@@ -0,0 +1,33 @@
+let
+    lsalphas = [0.5,0.5,0.49995,0.9,0.5]
+
+    f(x) = vecdot(x,x)
+    function g!(x, out)
+        out[:] = 2x
+    end
+
+    df = LineSearches.DifferentiableFunction(f,g!)
+
+
+
+    for (i, linesearch!) in enumerate(lsfunctions)
+        println("Testing $(string(linesearch!))")
+        x = [-1., -1.]
+        xtmp = copy(x)
+        grtmp = similar(x)
+        phi0 = df.fg!(x, grtmp)
+        p = -grtmp
+        dphi0 = dot(p, grtmp)
+
+        lsr = LineSearchResults(eltype(x))
+        push!(lsr, 0.0, phi0, dphi0)
+
+        alpha = 1.0
+        mayterminate = false
+
+        alpha, f_update, g_update = linesearch!(df, x, p, xtmp, grtmp, lsr, alpha, mayterminate)
+        #xnew = x + alpha*p
+
+        @test_approx_eq alpha lsalphas[i]
+    end
+end
