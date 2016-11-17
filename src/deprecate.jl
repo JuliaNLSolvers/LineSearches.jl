d5a7bfaa24b615311a11e3fc8e0444cd3a1c5c45
Asbjørn Nilsen Riseth <anriseth@gmail.com>
2016-11-17 09:38:44 +0000

    Deprecate interpbacktrack to bt2!

--- src/deprecate.jl
+++ src/deprecate.jl
@@ -3,6 +3,7 @@
 @deprecate hz_linesearch! hagerzhang!
 @deprecate mt_linesearch! morethuente!
 @deprecate backtracking_linesearch! backtracking!
-@deprecate interpbacktrack_linesearch! backtracking!
+@deprecate interpbacktrack_linesearch! bt2!
 @deprecate interpolating_linesearch! strongwolfe!
 @deprecate LinesearchException LineSearchException
+@deprecate interpbacktrack! bt2!
