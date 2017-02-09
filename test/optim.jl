5c91ffe4b3ab9f028640682d9e58c0c555efc8ae
Asbjørn Nilsen Riseth <anriseth@gmail.com>
2017-02-09 14:05:43 +0000

    Use true gradients in tests

--- test/optim.jl
+++ test/optim.jl
@@ -3,17 +3,13 @@ import Optim
 
 let
     for ls in lsfunctions
-        if ls == LineSearches.basic!
-            continue
-        end
-        #finc = (ls == basic!)
-        opts = Optim.Options(autodiff = true)#,
-                             #allow_f_increases = finc)
+        opts = Optim.Options(allow_f_increases = true)
 
         println("\nTesting $(string(ls))")
         for (name, prob) in Optim.UnconstrainedProblems.examples
             if prob.isdifferentiable
-                res = Optim.optimize(prob.f, prob.initial_x, Optim.Newton(linesearch = ls), opts)
+                res = Optim.optimize(prob.f, prob.g!, prob.h!,
+                                     prob.initial_x, Optim.Newton(linesearch = ls), opts)
                 println("$(name):\titerations = $(res.iterations)\tf_calls = $(Optim.f_calls(res))\tg_calls = $(Optim.g_calls(res))")
                 @assert Optim.minimum(res) <  prob.f(prob.solutions) + 1e-2
             end
