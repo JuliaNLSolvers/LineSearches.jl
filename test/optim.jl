32edb353eac91d83106691404a037a2d3bec9ad7
Asbjørn Nilsen Riseth <anriseth@gmail.com>
2017-02-09 15:00:43 +0000

    skip basic until allow_f_increases

--- test/optim.jl
+++ test/optim.jl
@@ -3,7 +3,11 @@ import Optim
 
 let
     for ls in lsfunctions
-        opts = Optim.Options(allow_f_increases = true)
+        # TODO: add basic! when allow_f_increases is tagged in Optim
+        if ls == LineSearches.basic!
+            continue
+        end
+        opts = Optim.Options()#allow_f_increases = true)
 
         println("\nTesting $(string(ls))")
         for (name, prob) in Optim.UnconstrainedProblems.examples
