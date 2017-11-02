5950d3e79a47218e3391e1fc27d4a1dde2ca2896
Asbjørn Nilsen Riseth <anriseth@gmail.com>
2017-11-02 15:45:25 +0000

    Deprecate

--- src/deprecate.jl
+++ src/deprecate.jl
@@ -1,3 +1,41 @@
 # Deprecation warnings
 
-# Clean as of LineSearches 3.0
+# alphatry and alphainit has been replaced with InitialHagerZhang
+
+const dep_alphatry = Ref(false)
+const dep_alphainit = Ref(false)
+
+function alphainit(alpha::Real,
+                   x::Array{T},
+                   gr::Array,
+                   f_x::Real,
+                   psi0::T = convert(T,0.01)) where T
+    if dep_alphainit[] == false
+      warn("`alphainit` is deprecated, use `InitialHagerZhang()` instead")
+      dep_alphainit[] = true
+    end
+    if isnan(alpha)
+        alpha = _hzI0(x,gr,f_x,psi0)
+    end
+    alpha
+end
+
+function alphatry(alpha::T,
+                  df,
+                  x::Array,
+                  s::Array,
+                  xtmp::Array,
+                  lsr::LineSearchResults,
+                  psi1::Real = convert(T,0.2),
+                  psi2::Real = convert(T,2),
+                  psi3::Real = convert(T,0.1),
+                  iterfinitemax::Integer = ceil(Integer, -log2(eps(T))),
+                  alphamax::Real = convert(T, Inf),
+                  verbose::Bool = false) where T
+   if dep_alphatry[] == false
+      warn("`alphatry` is deprecated, use `InitialHagerZhang()` instead")
+      dep_alphatry[] = true
+    end
+
+    _hzI12(alpha, df, x, s, xtmp, lsr, psi1, psi2, psi3, iterfinitemax, alphamax, verbose)
+end
