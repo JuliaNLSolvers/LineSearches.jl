57df7ffb99b0be6fbe5a2201f15673bfc943a2a8
Asbjørn Nilsen Riseth <anriseth@gmail.com>
2018-04-25 09:34:27 +0100

    Make InitialStatic type stable

--- src/initialguess.jl
+++ src/initialguess.jl
@@ -13,11 +13,12 @@ is scaled with the `l_2` norm of the step direction.
 end
 
 function (is::InitialStatic{T})(ls, state, phi_0, dphi_0, df) where T
-    if is.scaled == true && (ns = vecnorm(state.s)) > zero(T)
+    PT = promote_type(T, eltype(state.s))
+    if is.scaled == true && (ns = vecnorm(state.s)) > zero(PT)
         # TODO: Type instability if there's a type mismatch between is.alpha and ns?
-        state.alpha = min(is.alpha, ns) / ns
+        state.alpha = PT(min(is.alpha, ns)) / ns
     else
-        state.alpha = is.alpha
+        state.alpha = PT(is.alpha)
     end
 end
 
