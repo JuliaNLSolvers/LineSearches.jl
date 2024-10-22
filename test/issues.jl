71b72e1fd23f76e1033b1f0f2a3a78e2522dbbe2
Tim Holy <tim.holy@gmail.com>
2024-10-22 08:24:00 -0500

    Add an attempted flatness-convergence test
    The flatness check in HagerZhang was intended to prevent excessive
    iteration in cases where the objective function is flat to within
    numerical precision. This test is a poor attempt at capturing the issue.
    HZ takes more iterations than any other linesearch algorithm, but it's
    still a far cry from real-world cases I've seen where the linesearch
    exits from the iteration limit.

--- test/issues.jl
+++ test/issues.jl
@@ -19,7 +19,7 @@ gv = similar(x)
 ϕ0 = fg!(gv, x)
 s = -1*gv
 dϕ0 = dot(gv, s)
-println(ϕ0, ", ", dϕ0)
+# println(ϕ0, ", ", dϕ0)
 
 # Univariate line search functions
 ϕ(α) = f(x .+ α.*s)
@@ -36,3 +36,35 @@ end
 res = (StrongWolfe())(ϕ, dϕ, ϕdϕ, α0, ϕ0, dϕ0)
 @test res[2] > 0
 @test res[2] == ϕ(res[1])
+
+# Flatness check in HagerZhang
+function makeϕdϕ(a)
+    @assert axes(a) == (1:2,)
+    A = a*a'
+    f(x) = x'*A*x/2   # Hessian has one positive and one zero eigenvalue (in exact arithmetic)
+    df(x) = A*x
+    x0 = [a[2], -a[1]]
+    d = -x0 / 2
+    ϕ(α) = f(x0 + α*d)
+    dϕ(α) = dot(df(x0 + α*d), d)
+    ϕdϕ(α) = (ϕ(α), dϕ(α))
+    return ϕ, dϕ, ϕdϕ
+end
+
+@testset "Flatness" begin
+    cache = LineSearchCache{Float64}()
+    lsalgs =  (HagerZhang(; cache=cache), StrongWolfe(; cache=cache), MoreThuente(; cache=cache),
+               BackTracking(; cache=cache), BackTracking(; order=2, cache=cache) )
+
+    n = 0
+    while n < 10
+        ϕ, dϕ, ϕdϕ = makeϕdϕ(randn(2))
+        ϕ0, dϕ0 = ϕdϕ(0)
+        dϕ0 < -eps(abs(ϕ0)) || continue    # any "slope" is just roundoff error, but we want roundoff that looks like descent
+        n += 1
+        for ls in lsalgs
+            res = ls(ϕ, dϕ, ϕdϕ, 1.0, ϕ(0.0), dϕ(0.0))
+            @test length(cache.alphas) < 10   # really should be < 5
+        end
+    end
+end
