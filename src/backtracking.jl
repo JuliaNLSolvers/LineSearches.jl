26839d68db11bb5756d7f6ca40fa2318b68e707d
Patrick Kofod Mogensen <patrick.mogensen@gmail.com>
2026-05-22 17:19:37 +0200

    Update docs (#204)

--- src/backtracking.jl
+++ src/backtracking.jl
@@ -1,12 +1,14 @@
 """
-`BackTracking` specifies a backtracking line-search that uses
-a quadratic or cubic interpolant to determine the reduction in step-size.
-E.g.,
-if f(α) > f(0) + c₁ α f'(0), then the quadratic interpolant of
-f(0), f'(0), f(α) has a minimiser α' in the open interval (0, α). More strongly,
-there exists a factor ρ = ρ(c₁) such that α' ≦ ρ α.
+    BackTracking(; c_1=1e-4, ρ_hi=0.5, ρ_lo=0.1, iterations=1_000, order=3, maxstep=Inf, cache=nothing)
 
-This is a modification of the algorithm described in Nocedal Wright (2nd ed), Sec. 3.5.
+A backtracking line-search that uses a quadratic or cubic interpolant to
+determine the reduction in step-size. E.g., if `f(α) > f(0) + c₁ α f'(0)`, then
+the quadratic interpolant of `f(0)`, `f'(0)`, `f(α)` has a minimiser `α'` in the
+open interval `(0, α)`. More strongly, there exists a factor `ρ = ρ(c_1)` such
+that `α' ≦ ρ α`.
+
+This is a modification of the algorithm described in Nocedal Wright (2nd ed),
+Sec. 3.5.
 """
 @kwdef struct BackTracking{TF, TI} <: AbstractLineSearch
     c_1::TF = 1e-4
--- src/static.jl
+++ src/static.jl
@@ -1,7 +1,11 @@
 """
-`Static`: defines a static linesearch which returns the initial step length.
+    Static()
 
-`Static` is intended for methods with well-scaled updates; i.e. Newton, on well-behaved problems.
+A static linesearch that accepts the initial step length unchanged (no parameters
+to configure).
+
+`Static` is intended for methods with well-scaled updates; i.e. Newton, on
+well-behaved problems.
 """
 struct Static <: AbstractLineSearch end
 
